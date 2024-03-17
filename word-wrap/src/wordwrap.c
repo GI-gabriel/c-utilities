93e5f271257592b8d47631b447646ec8ed6f1f8f
GI-gabriel <161953323+GI-gabriel@users.noreply.github.com>
2024-03-16 22:45:06 -0300

    Change tab character to spaces in wordwrap.c

--- word-wrap/src/wordwrap.c
+++ word-wrap/src/wordwrap.c
@@ -24,130 +24,123 @@ const char *title = "";
 const char *indentSym = " ";
 
 void readFile(const char *filename, char *string, int start) {
-	FILE *file_ = fopen(filename, "r");
-	if (file_ == NULL) {
-		printf("Cannot find file\n");
-		exit(1);
-	}
+    FILE *file_ = fopen(filename, "r");
+    if (file_ == NULL) {
+        printf("Cannot find file\n");
+        exit(1);
+    }
 
-	int i = start;
-	char c = fgetc(file_);
-	while (c != EOF && i < MAX_BUFFER - 1) {
-		string[i++] = (char) c;
-		c = fgetc(file_);
-	}
-	string[i] = '\0';
+    int i = start;
+    char c = fgetc(file_);
+    while (c != EOF && i < MAX_BUFFER - 1) {
+        string[i++] = (char) c;
+        c = fgetc(file_);
+    }
+    string[i] = '\0';
 
-	if (fclose(file_) == EOF) {
-		printf("Cannot close file\n");
-		exit(1);
-	}
-	file_ = NULL;
+    if (fclose(file_) == EOF) {
+        printf("Cannot close file\n");
+        exit(1);
+    }
+    file_ = NULL;
 }
 
 void charReplace(char *string, char from, char to) {
-	for (int i = 0; string[i] != '\0'; i++) {
-		if (string[i] == from)
-			string[i] = to;
-	}
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == from)
+            string[i] = to;
+    }
 }
 
 void writeFile(const char *filename, char *string) {
-	FILE *file_ = fopen(filename, "w");
-	if (file_ == NULL) {
-		printf("Cannot find file\n");
-		exit(1);
-	}
+    FILE *file_ = fopen(filename, "w");
+    if (file_ == NULL) {
+        printf("Cannot find file\n");
+        exit(1);
+    }
 
-	for (int i = 0; i < strlen(string); i++)
-		if (fputc(string[i], file_) == EOF)
-			break;
+    for (int i = 0; i < strlen(string); i++)
+        if (fputc(string[i], file_) == EOF)
+            break;
 
-	if (fclose(file_) == EOF) {
-		printf("Cannot close file\n");
-		exit(1);
-	}
-	file_ = NULL;
+    if (fclose(file_) == EOF) {
+        printf("Cannot close file\n");
+        exit(1);
+    }
+    file_ = NULL;
 }
 
 void wrap(char *string, int max) {
-	int i, l, w;
+    int i, l, w;
 
-	i = l = w = 0;
-	while (string[i] != '\0') {
-		if (i > 0 && string[i] == ' ' && string[i - 1] != ' ')
-			w = i;
+    i = l = w = 0;
+    while (string[i] != '\0') {
+        if (i > 0 && string[i] == ' ' && string[i - 1] != ' ')
+            w = i;
 
-		if (l >= max) {
-			string[w] = '\n';
-			i = w + 1;
-			l = 0;
-		} else {
-			i++;
-			l++;
-		}
-	}
+        if (l >= max) {
+            string[w] = '\n';
+            i = w + 1;
+            l = 0;
+        } else {
+            i++;
+            l++;
+        }
+    }
 }
 
 void indent(char *to, const char *from, const char *indent) {
-	int i, j, k;
+    int i, j, k;
 
-	i = j = 0;
-	while (from[j] != '\0' && i < MAX_BUFFER) {
+    i = j = 0;
+    while (from[j] != '\0' && i < MAX_BUFFER) {
 
-		k = 0;
-		while (indent[k] != '\0' && i < MAX_BUFFER)
-			to[i++] = indent[k++];
+        k = 0;
+        while (indent[k] != '\0' && i < MAX_BUFFER)
+            to[i++] = indent[k++];
 
-		while (from[j] != '\n' && i < MAX_BUFFER)
-			to[i++] = from[j++];
+        while (from[j] != '\n' && i < MAX_BUFFER)
+            to[i++] = from[j++];
 
-		to[i++] = from[j++];
-	}
+        to[i++] = from[j++];
+    }
 }
 
 void usage() {
-	printf(
-			"Word wrap tool to limit line width to predefined value.\n\n"
-					"Usage: wordwrap [OPTION]\n\n"
-					"OPTION\n"
-					"    -1: Create intermediate text file ready to add break lines (you must\n"
-					"        do it manually).\n"
-					"    -2: Starting from properly edited intermediate text file, with  break\n"
-					"        at desired locations, this command will limit line width by adding\n"
-					"        word wrap.\n\n"
-					"FILES\n"
-					"    You must provide input.txt in same folder.\n"
-					"    Option -1 reads input.txt and creates intermediate.txt for you to edit.\n"
-					"    Option -2 reads intermediate.txt and produces output.txt.\n");
-	exit(2);
+    printf(
+            "Word wrap tool to limit line width to predefined value.\n\n"
+                    "Usage: wordwrap [OPTION]\n\n"
+                    "OPTION\n"
+                    "    -1: Create intermediate text file ready to add break lines (you must\n"
+                    "        do it manually).\n"
+                    "    -2: Starting from properly edited intermediate text file, with  break\n"
+                    "        at desired locations, this command will limit line width by adding\n"
+                    "        word wrap.\n\n"
+                    "FILES\n"
+                    "    You must provide input.txt in same folder.\n"
+                    "    Option -1 reads input.txt and creates intermediate.txt for you to edit.\n"
+                    "    Option -2 reads intermediate.txt and produces output.txt.\n");
+    exit(2);
 }
 
 int main(int argc, char *argv[]) {
-	// Open input file, then get input string.
-	// Replace all line breaks but the last one, with spaces.
-	// Add "Description: " at the beginning;
-	// Count characters until predefined maximum, then go back to the last
-	// word and replace space by newline.
-	// Repeat last operation until end-of-file.
+    char stringStep1[MAX_BUFFER] = { '\0' };
+    char stringStep2[MAX_BUFFER] = { '\0' };
 
-	char stringStep1[MAX_BUFFER] = { '\0' };
-	char stringStep2[MAX_BUFFER] = { '\0' };
-
-	if (argc == 2) {
-		if (!strcmp(argv[1], "-1")) {
-			strcpy(stringStep1, title);
-			readFile(inputFileName, stringStep1, strlen(title));
-			charReplace(stringStep1, '\n', ' ');
-			writeFile(intermFileName, stringStep1);
-		} else if (!strcmp(argv[1], "-2")) {
-			readFile(intermFileName, stringStep1, 0);
-			wrap(stringStep1, MAX_WIDTH - strlen(indentSym));
-			indent(stringStep2, stringStep1, indentSym);
-			writeFile(outputFileName, stringStep2);
-		} else
-			usage();
-	} else
-		usage();
+    if (argc == 2) {
+        if (!strcmp(argv[1], "-1")) {
+            strcpy(stringStep1, title);
+            readFile(inputFileName, stringStep1, strlen(title));
+            charReplace(stringStep1, '\n', ' ');
+            writeFile(intermFileName, stringStep1);
+        } else if (!strcmp(argv[1], "-2")) {
+            readFile(intermFileName, stringStep1, 0);
+            wrap(stringStep1, MAX_WIDTH - strlen(indentSym));
+            indent(stringStep2, stringStep1, indentSym);
+            writeFile(outputFileName, stringStep2);
+        } else
+            usage();
+    } else
+        usage();
 }
 
